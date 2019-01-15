baf64aa48f9cea9b63614ceb673cd6b2ff43e393
mp49211 <39171680+mp49211@users.noreply.github.com>
2019-01-15 15:37:35 +0100

    Add files via upload

---
+++ KozmetickiClassLibrary/ViewModels/ArtikliVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KozmetickiClassLibrary.ViewModels
+{
+    public class ArtikliVM
+    {
+        public ArtikliVM()
+        {
+            Arts = new List<artikl>();
+            Dict = new Dictionary<int, int>();
+            Kols = new List<int>();
+        }
+        public List<artikl> Arts { get; set; }
+        public List<int> Kols { get; set; }
+        public Dictionary<int, int> Dict { get; set; }
+    }
+}
---
+++ KozmetickiClassLibrary/ViewModels/ZaposlenikAdresaVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KozmetickiClassLibrary.ViewModels
+{
+    public class ZaposlenikAdresaVM
+    {
+        public ZaposlenikAdresaVM() {
+        }
+        public zaposlenik zaposlenik { get; set; }
+        public adresa adresa { get; set; }
+        public List<UslugaVM> usluge { get; set; }
+    }
+}
